00d61f8d5e3538a80645122e1bdbe3c3650a54c5
mdsumner <mdsumner@gmail.com>
2013-08-23 23:47:31 +1000

    experiment with staging, added some comments

--- ffraster.R
+++ ffraster.R
@@ -1,22 +1,28 @@
 mat <- volcano
 
+## load required packages
 library(raster)
 library(ff)
 r <- brick(raster(mat), raster(mat), raster(mat))
 ##r <- brick(arr)
 ##r <- raster(mat)
 
+## set up file names to use for rasterfile
 datafile <- sprintf("%s.gri", tempfile())
 hdrfile <- gsub("gri$", "grd", datafile)
 
+## create rasterfile, we only want the hdrfile part
 rmat <- writeRaster(r, filename = hdrfile, overwrite = TRUE)
 
+## map types between ff and raster
 vm <- c(FLT4S = "single", FLT8S = "double")[rmat@file@datanotation]
 
+## set up data dimensions and order
 vdim <- dim(r)
 if(vdim[length(vdim)] < 2) vdim <- vdim[length(vdim)-1]
 dimo <- seq_along(vdim)[c(seq_along(vdim)[-1], 1)]
 
+## create ff object
 ffmat <- ff(dim = vdim, vmode = vm, dimorder = dimo, filename = datafile, readonly = TRUE)
 
 
