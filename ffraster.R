92e4f4924d382e2a073c5fd26372a8cb8ced71df
Michael Sumner <mdsumner@gmail.com>
2014-07-17 07:25:23 +0000

    practical example

--- ffraster.R
+++ ffraster.R
@@ -3,10 +3,19 @@ mat <- volcano
 ## load required packages
 library(raster)
 library(ff)
-r <- brick(raster(mat), raster(mat), raster(mat))
+##r <- brick(raster(mat), raster(mat), raster(mat))
 ##r <- brick(arr)
 ##r <- raster(mat)
 
+library(raadtools)
+
+newfile <- "ice_series_daily_1991"
+
+dts <- icefiles()$date
+dts <- dts[dts > as.POSIXct("1991-01-01 00:00:00")]
+nl <- length(dts)
+r <- readice(dts[1])
+dts <- dts[seq_len(nl)]
 ## set up file names to use for rasterfile
 datafile <- sprintf("%s.gri", tempfile())
 hdrfile <- gsub("gri$", "grd", datafile)
@@ -19,15 +28,29 @@ vm <- c(FLT4S = "single", FLT8S = "double")[rmat@file@datanotation]
 
 ## set up data dimensions and order
 vdim <- dim(r)
-if(vdim[length(vdim)] < 2) vdim <- vdim[length(vdim)-1]
+vdim[3] <- nl
+if(vdim[length(vdim)] < 2) vdim <- vdim[-(length(vdim))]
 dimo <- seq_along(vdim)[c(seq_along(vdim)[-1], 1)]
 
 ## create ff object
-ffmat <- ff(dim = vdim, vmode = vm, dimorder = dimo, filename = datafile, readonly = TRUE)
+ffmat <- ff(dim = vdim, vmode = vm, dimorder = dimo, filename = sprintf("%s.gri", newfile), readonly = FALSE, overwrite = TRUE)
+for (i in seq_along(dts)) {
+  r <- readice(dts[i])
+  if (nl == 1) {
+    ffmat[,] <- as.matrix(r) 
+  } else {
+    ffmat[,,i] <- as.matrix(r)
+  }
+  if (i %% 500 == 0) print(i)
+}
+##ffarr <- ff(array(c(mat, mat, mat), vdim), dim = vdim, vmode = vm, dimorder = dimo, filename = sprintf("%s.gri", newfile))
+txt <- readLines(hdrfile)
+ln <- grep("nbands", txt)
+txt[ln] <- gsub("1", as.character(nl), txt[ln])
+writeLines(txt, sprintf("%s.grd", newfile))
 
 
-ffarr <- ff(array(c(mat, mat, mat), vdim), dim = vdim, vmode = vm, dimorder = dimo, filename = "afile.gri")
-file.copy(hdrfile, "afile.grd")
+#file.copy(hdrfile, sprintf("%s.grd", newfile))
 
 
 
